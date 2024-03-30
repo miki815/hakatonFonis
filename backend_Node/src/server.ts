bb8f4cd1113ecf14dfc79bfb9b0cb13ebfb84a9c
Nemanja <krivokapicnemanja0@gmail.com>
2024-03-30 12:54:29 +0100

    Baza

--- backend_Node/src/server.ts
+++ backend_Node/src/server.ts
@@ -1,6 +1,24 @@
 import express from 'express';
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
+
+
 
 const app = express();
+app.use(cors());
+//app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+mongoose.connect('mongodb://127.0.0.1:27017/fonis')
+const connection=mongoose.connection;
+connection.once('open', ()=>{
+    console.log('db connection ok')
+})
+
+const router=express.Router();
+app.use('/', router)
+
 app.listen(4000, () => console.log(`Express server running on port 4000`));
