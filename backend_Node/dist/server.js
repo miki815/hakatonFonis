72dc459b7cd853b8f6b5af505ac58c3bb1cd2a1c
Nemanja <krivokapicnemanja0@gmail.com>
2024-03-30 13:28:09 +0100

    Pocetno

--- backend_Node/dist/server.js
+++ backend_Node/dist/server.js
@@ -4,6 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use((0, cors_1.default)());
+//app.use(bodyParser.json());
+app.use(body_parser_1.default.json({ limit: '50mb' }));
+app.use(body_parser_1.default.urlencoded({ limit: '50mb', extended: true }));
+mongoose_1.default.connect('mongodb://127.0.0.1:27017/fonis');
+const connection = mongoose_1.default.connection;
+connection.once('open', () => {
+    console.log('db connection ok');
+});
+const router = express_1.default.Router();
+app.use('/', router);
 app.listen(4000, () => console.log(`Express server running on port 4000`));
