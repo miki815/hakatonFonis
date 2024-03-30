0eca688bf5b8925cb20467030bc346c69ec22e85
mikaajlo <mihajlorajic.mr@gmail.com>
2024-03-30 16:06:13 +0100

    learn

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KvizComponent } from './kviz/kviz.component';
 import { LoginComponent } from './login/login.component';
-import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LearnComponent } from './learn/learn.component';
 
 const routes: Routes = [
-  { path: "kviz", component: KvizComponent },
-  { path: "login", component: LoginComponent },
+  {path: "kviz", component: KvizComponent },
   {path: 'login', component: LoginComponent},
-  {path: 'homepage', component: HomepageComponent}
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'learn', component: LearnComponent}
 ];
 
 @NgModule({
---
+++ frontend/frontend/src/app/learn/learn.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-learn',
+  standalone: true,
+  imports: [],
+  templateUrl: './learn.component.html',
+  styleUrl: './learn.component.css'
+})
+export class LearnComponent {
+  constructor(private router: Router) {}
+
+  game(){
+    this.router.navigate(['../game']);
+  }
+  kviz(){
+    this.router.navigate(['../kviz']);
+  }
+
+}
