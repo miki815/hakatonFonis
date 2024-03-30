3fd9fdf543c5b651097df6a161d578b2ddba232e
mikaajlo <mihajlorajic.mr@gmail.com>
2024-03-30 14:10:08 +0100

    homepage

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  /*{path: "register", component: null},*/
+  {path: 'login', component: LoginComponent},
+  {path: 'homepage', component: HomepageComponent}
 ];
 
 @NgModule({
