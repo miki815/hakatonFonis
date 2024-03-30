c583d17b3ce06bb668c64b6bf0ce9385a8bb2611
ru210540d <ru210540d@student.etf.bg.ac.rs>
2024-03-30 15:36:42 +0100

    dodata game componenta

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/game', pathMatch: 'full' },
+  { path: 'game', component: GameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
